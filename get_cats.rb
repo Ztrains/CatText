f55a261694356354c51e7d7ff2bf0e754d2cb272
Zachary <zachary.t.rich@gmail.com>
2016-11-19 18:29:45 -0600

    removed useless file

--- get_cats.rb
+++
@@ -1,9 +0,0 @@
-require 'httparty'
-require 'pp'
- 
-def get_url
-	url = 'http://thecatapi.com/api/images/get?format=xml&results_per_page=1'
-	response = HTTParty.get(url)
-	pic = response.parsed_response['response']['data']['images']['image'].first
-	url = pic.last
-end
