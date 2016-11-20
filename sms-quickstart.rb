5c01ae2e1d9acdc38ec24e4d7e63ee95a6f49f90
Zachary <zachary.t.rich@gmail.com>
2016-11-20 08:37:59 -0600

    Tried to fix some things

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -17,18 +17,17 @@ end
 get '/sms-quickstart' do
 	
 	session[:step] = 0 if params['Body'] == "Clear"
-	session[:name] = 0 if params['Body'] == "Clear"
 
 	session[:step] ||= 0 
 	session[:step] += 1
 
   twiml = Twilio::TwiML::Response.new do |r|
-    if params['Body'].downcase == 'send cats'
+    if params['Body'].downcase.include? 'send cats'
     	r.Message do |message|
     		message.Media get_url
     		message.Body "Thanks for texting in to CatPics! You can always reply with \'another\' to get more! Or, tell me how the cat looked!"
     	end
-    elsif (params['Body'] == 'another') || (params['Body'] == 'Another')
+    elsif params['Body'].downcase.include? 'another'
     	session[:step] = 0
     	
     	r.Message do |message|
@@ -36,14 +35,9 @@ get '/sms-quickstart' do
     		message.Body "Here's another, reply with another again to get more! Or, tell me how the cat looked!"
     	end
     elsif session[:step] == 1
-    	session[:city] = params['FromCity']
-    	session[:phonenumber] = params['From']
-    	File.open('replies.txt', 'a') { |f| 
-    		f << "Reply received from number #{session[:phonenumber]}, 
-    		they haven't put in their name yet, 
-    		in city #{session[:city]}, 
-    		and they sent: #{session[:body]}\n\n\n" 
-    	}
+    	#session[:city] = params['FromCity']
+    	#session[:phonenumber] = params['From']
+    	
     	if session[:name] == nil
     		r.Message "Thanks for sharing. What's your name?"
     	else
@@ -51,29 +45,15 @@ get '/sms-quickstart' do
     		session[:step] == 2
     	end
     elsif session[:step] == 2
-    	session[:name] = params['Body']
+    	#session[:name] = params['Body']
 
     	if session[:name] == nil
     		session[:name] = params['Body']
     	end
 
-    	File.open('replies.txt', 'a') { |f| 
-    		f << "Reply received from number #{session[:phonenumber]}, 
-    		from name #{session[:name]}, 
-    		in city #{session[:city]}, 
-    		and they sent their name.\n\n\n" 
-    	}
-
-    	r.Message "Nice to meet you #{session[:name]}, unfortunately from this number I only send out cat pictures. You can always reply with \'another\' to see more cats! Thanks for looking at my cats though!"
+    	r.Message "Nice to meet you #{session[:name]}, unfortunately from this number I only send out cat pictures. You can always reply with \'another\' to see more cats!"
     else
-    	session[:body] = params['Body']
-
-    	File.open('replies.txt', 'a') { |f| 
-    		f << "Reply received from number #{session[:phonenumber]},
-    		from name #{session[:name]},
-    		in city #{session[:city]},
-    		and they sent: #{session[:body]}\n\n\n" 
-    	}
+    	#session[:body] = params['Body']
 
     	r.Message "Hi again, #{session[:name]}, but if you remember I can't reply back to you. Sorry! You can always reply with \'another\' to see more cats, however!"
     end
